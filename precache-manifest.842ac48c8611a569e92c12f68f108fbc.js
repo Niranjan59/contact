51f30e82c36cd007ccd2fc52a655d7d7c9e4240e
Niranjan59 <niranjanpun59@gmail.com>
2022-10-02 21:54:29 +0545

    Updates

---
+++ precache-manifest.842ac48c8611a569e92c12f68f108fbc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ca9391571e8b341867c",
+    "url": "/contact/static/js/main.6ca93915.chunk.js"
+  },
+  {
+    "revision": "73d3c09f02479628f9ef",
+    "url": "/contact/static/js/1.73d3c09f.chunk.js"
+  },
+  {
+    "revision": "fbf82b8ce2fdcf534fbc",
+    "url": "/contact/static/js/runtime~main.fbf82b8c.js"
+  },
+  {
+    "revision": "5dca5aebc5924da10aba2849d90f2f23",
+    "url": "/contact/index.html"
+  }
+];
